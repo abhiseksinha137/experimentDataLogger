5715795b4a11e4a0f830c4787d1c3e7a4546e797
Abhisek Sinha <sinha.abhisek92@gmail.com>
2023-12-20 14:37:32 +0530

    First Commit

---
+++ experimentDataLogger/Form1.Designer.vb
@@ -0,0 +1,174 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.btnRefresh = New System.Windows.Forms.Button()
+        Me.btnOpen = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnLoad = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnRemove = New System.Windows.Forms.Button()
+        Me.DGV1 = New System.Windows.Forms.DataGridView()
+        Me.Parameter = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Value = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DGV1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'btnRefresh
+        '
+        Me.btnRefresh.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnRefresh.Location = New System.Drawing.Point(269, 12)
+        Me.btnRefresh.Name = "btnRefresh"
+        Me.btnRefresh.Size = New System.Drawing.Size(74, 23)
+        Me.btnRefresh.TabIndex = 7
+        Me.btnRefresh.Text = "Refresh"
+        Me.btnRefresh.UseVisualStyleBackColor = True
+        '
+        'btnOpen
+        '
+        Me.btnOpen.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnOpen.Location = New System.Drawing.Point(84, 12)
+        Me.btnOpen.Name = "btnOpen"
+        Me.btnOpen.Size = New System.Drawing.Size(74, 23)
+        Me.btnOpen.TabIndex = 1
+        Me.btnOpen.Text = "Open"
+        Me.btnOpen.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnSave.Location = New System.Drawing.Point(6, 12)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(74, 23)
+        Me.btnSave.TabIndex = 0
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnLoad
+        '
+        Me.btnLoad.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnLoad.Location = New System.Drawing.Point(165, 12)
+        Me.btnLoad.Name = "btnLoad"
+        Me.btnLoad.Size = New System.Drawing.Size(97, 23)
+        Me.btnLoad.TabIndex = 2
+        Me.btnLoad.Text = "Load Template"
+        Me.btnLoad.UseVisualStyleBackColor = True
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnAdd.Location = New System.Drawing.Point(388, 12)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(74, 23)
+        Me.btnAdd.TabIndex = 8
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnClear.Location = New System.Drawing.Point(569, 12)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(92, 23)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Clear All"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnRemove
+        '
+        Me.btnRemove.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnRemove.Location = New System.Drawing.Point(469, 12)
+        Me.btnRemove.Name = "btnRemove"
+        Me.btnRemove.Size = New System.Drawing.Size(74, 23)
+        Me.btnRemove.TabIndex = 6
+        Me.btnRemove.Text = "Remove"
+        Me.btnRemove.UseVisualStyleBackColor = True
+        '
+        'DGV1
+        '
+        Me.DGV1.Anchor = CType((System.Windows.Forms.AnchorStyles.Left Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DGV1.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
+        Me.DGV1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGV1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Parameter, Me.Value})
+        Me.DGV1.Location = New System.Drawing.Point(6, 41)
+        Me.DGV1.Name = "DGV1"
+        Me.DGV1.Size = New System.Drawing.Size(655, 329)
+        Me.DGV1.TabIndex = 0
+        '
+        'Parameter
+        '
+        Me.Parameter.HeaderText = "Parameter"
+        Me.Parameter.Name = "Parameter"
+        '
+        'Value
+        '
+        Me.Value.HeaderText = "Value"
+        Me.Value.Name = "Value"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(673, 370)
+        Me.Controls.Add(Me.DGV1)
+        Me.Controls.Add(Me.btnRemove)
+        Me.Controls.Add(Me.btnLoad)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnRefresh)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.btnOpen)
+        Me.Controls.Add(Me.btnSave)
+        Me.Name = "Form1"
+        Me.Text = "Experimental Data Logger"
+        CType(Me.DGV1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents btnRefresh As Button
+    Friend WithEvents btnOpen As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents btnLoad As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnRemove As Button
+    Friend WithEvents DGV1 As DataGridView
+    Friend WithEvents Parameter As DataGridViewTextBoxColumn
+    Friend WithEvents Value As DataGridViewTextBoxColumn
+End Class
---
+++ experimentDataLogger/Form1.vb
@@ -0,0 +1,144 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Dim paramDict As New Dictionary(Of String, String)
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        If DGV1.RowCount < 2 Then
+            MsgBox("Nothing to Save")
+        Else
+            saveData()
+        End If
+    End Sub
+
+    Public Function saveData()
+        If updateDict() = 1 Then
+            writeDict(paramDict)
+        Else
+            MsgBox("Can't Save")
+        End If
+    End Function
+    Public Function updateDict()
+        Dim paramDictDummy As New Dictionary(Of String, String)
+        For i = 0 To DGV1.RowCount - 2
+            Dim param As String
+            Dim val As String
+            Try
+                param = DGV1.Rows(i).Cells(0).Value.ToString
+                val = DGV1.Rows(i).Cells(1).Value.ToString
+                paramDictDummy.Add(param, val)
+            Catch ex As Exception
+                Return 0
+            End Try
+            paramDict = paramDictDummy
+        Next
+
+        Return 1
+    End Function
+
+    Public Sub writeDict(ByVal dic As Dictionary(Of String, String))
+        Dim s As String = String.Join(vbNewLine, dic.Select(Function(pair) String.Format("{0}={1}", pair.Key, pair.Value)).ToArray())
+        Try
+            SaveFileDialog1.Filter = "TXT Files (*.txt*)|*.txt"
+            SaveFileDialog1.FileName = "untitled.txt"
+            SaveFileDialog1.InitialDirectory = "D:\VBNet"
+            If SaveFileDialog1.ShowDialog = DialogResult.OK Then
+                Using writer As StreamWriter = New StreamWriter(SaveFileDialog1.FileName)
+                    writer.Write(s)
+                End Using
+                MsgBox("Saved")
+            End If
+            'Using writer As StreamWriter = New StreamWriter("myfile.txt")
+            '    writer.Write(s)
+            'End Using
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        clearData()
+    End Sub
+    Public Sub clearData()
+        paramDict = New Dictionary(Of String, String)
+        DGV1.Rows.Clear()
+    End Sub
+
+
+
+    Private Sub btnOpen_Click(sender As Object, e As EventArgs) Handles btnOpen.Click
+        openDataFile()
+    End Sub
+    Public Function openDataFile() As Boolean
+
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            clearData()
+            Dim fileName As String = OpenFileDialog1.FileName
+            ' Create new StreamReader instance with Using block.
+            For Each line As String In File.ReadLines(fileName)
+                ' Read one line from file
+                Dim words As String() = line.Split(New Char() {"="c})
+                paramDict.Add(words(0), words(1))
+            Next line
+            updateDGV()
+            Return True
+        End If
+        Return False
+    End Function
+
+    Public Sub updateDGV()
+        DGV1.Rows.Clear()
+
+        For Each kvp As KeyValuePair(Of String, String) In paramDict
+            Dim param As String = kvp.Key
+            Dim val As String = kvp.Value
+            DGV1.Rows.Add(param, val)
+        Next
+        DGV1.Update()
+    End Sub
+
+    Private Sub btnRemove_Click(sender As Object, e As EventArgs) Handles btnRemove.Click
+        Dim sel As DataGridViewSelectedRowCollection = DGV1.SelectedRows
+        For Each row As DataGridViewRow In sel
+            Dim param As String = row.Cells(0).Value.ToString
+            Dim res As Boolean = paramDict.Remove(param)
+        Next
+        updateDGV()
+    End Sub
+
+    Private Sub btnLoad_Click(sender As Object, e As EventArgs) Handles btnLoad.Click
+        Dim paramDictDummy As Dictionary(Of String, String) = paramDict
+        Dim loadData As Boolean = openDataFile()
+        If loadData = True Then
+            For Each kvp As KeyValuePair(Of String, String) In paramDict
+                Dim param As String = kvp.Key
+                paramDictDummy(param) = " "
+            Next
+            paramDict = paramDictDummy
+            updateDGV()
+        End If
+
+    End Sub
+
+    Private Sub btnRefresh_Click(sender As Object, e As EventArgs) Handles btnRefresh.Click
+        DGV1.Refresh()
+        DGV1.Update()
+    End Sub
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DGV1.Columns(1).AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+        DGV1.Update()
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim isUpdated As Boolean = updateDict()
+        If isUpdated = False Then
+            MsgBox("Error! Null Value or Number of rows < 2")
+        End If
+    End Sub
+
+
+End Class
